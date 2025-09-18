c1e9df99930f3719354b3bc2c010b84e9f3c0be1
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-09-18 23:19:09 +0900

    Drill #3 2022184036 정우진 자주 쓰이는 배경 and 객체 draw를 함수로 만들어서 더 간편히 사용하자

--- character_moves.py
+++ character_moves.py
@@ -16,14 +16,20 @@ base_y = 90
 # 캐릭터가 현재 서있는 위치 정보
 x = base_x
 y = base_y
+
+# 배경 그리기
+def draw_boy_and_background(x : float, y : float):
+    clear_canvas()
+    glass.draw_now(400,30)
+    character.draw_now(x,y)
+    update_canvas()
+
 # 캐릭터 오른쪽으로 이동
 def move_right(amount):
     global x
     global y
     for i in range(0, amount, 50):
-        clear_canvas_now()
-        glass.draw_now(400, 30)
-        character.draw_now(x, y)
+        draw_boy_and_background(x, y)
         x += 50
         delay(0.1)
     pass
@@ -35,9 +41,7 @@ def move_diagonal_left_up(amount):
     theta = 60
 
     for i in range(0,amount,50):
-        clear_canvas_now()
-        glass.draw_now(400, 30)
-        character.draw_now(x, y)
+        draw_boy_and_background(x, y)
         x = x - 50 * math.cos(math.radians(theta))
         y = y + 50 * math.sin(math.radians(theta))
         delay(0.1)
@@ -49,21 +53,19 @@ def move_diagonal_right_down(amount):
     global y
     theta = 300
     for i in range(0,amount,50):
-        clear_canvas_now()
-        glass.draw_now(400, 30)
-        character.draw_now(x, y)
+        draw_boy_and_background(x, y)
         x = x - 50 * math.cos(math.radians(theta))
         y = y + 50 * math.sin(math.radians(theta))
         delay(0.1)
     pass
 
 def triangle_move():
-    clear_canvas_now()
+
     move_right(200)
     move_diagonal_left_up(400)
     move_diagonal_right_down(400)
     move_right(200)
-    delay(0.1)
+
     pass
 
 def rectangle_move():
@@ -83,3 +85,5 @@ while True:
     rectangle_move()
     circle_move()
     pass
+
+close_canvas()
