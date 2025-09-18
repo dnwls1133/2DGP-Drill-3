004eb4703eaea7458a629ea454111473935ec0b8
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-09-18 20:39:36 +0900

    Drill #3 2022184036 정우진 무한 반복을 위한 루프구성

--- character_moves.py
+++ character_moves.py
@@ -1,7 +1,23 @@
 from pico2d import *
 
+
+def triangle_move():
+    pass
+
+def rectangle_move():
+    pass
+
+def circle_move():
+    pass
+
 open_canvas()
-
+# 삼각 운동
+# 사각 운동
+# 원 운동
+# 번갈아가며 무한 반복
 # fill here
-
-close_canvas()
+while True:
+    triangle_move()
+    rectangle_move()
+    circle_move()
+    pass
